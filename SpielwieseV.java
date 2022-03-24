d992088c7e64eb6d37aecfea61b54faa206331d0
leonwillwersch <101930729+leonwillwersch@users.noreply.github.com>
2022-03-24 17:34:49 +0100

    Add files via upload

---
+++ SpielwieseV.java
@@ -0,0 +1,16 @@
+package PR1Crahkurs;
+
+public class SpielwieseV {
+
+	public static void main(String[] args) {
+		int a = 14;
+		int b = 5;
+		int c = -a---b;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+
+
+	}
+
+}
