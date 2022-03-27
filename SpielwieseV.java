b269faf24b5cb242591bddff5cf51800a819aaee
leonwillwersch <2123100@stud.hs-mannheim.de>
2022-03-27 14:28:39 +0200

    Erster Commit

--- SpielwieseV.java
+++ SpielwieseV.java
@@ -3,16 +3,7 @@ package PR1Crahkurs;
 public class SpielwieseV {
 
 	public static void main(String[] args) {
-		int a = 14;
-		int b = 5;
-		int c = -a---b;
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
-		for (int i = 0; i<10; i++) {
-			System.out.println("Hello World");
-		}
-		System.out.print("Hi");
+		System.out.println("Hello World");
 
 
 	}
