1ddbd7f850b9615f19a23fe2f6e025311bcbcbde
leonwillwersch <2123100@stud.hs-mannheim.de>
2022-03-27 21:01:04 +0200

    Test Commit

--- SpielwieseV.java
+++ SpielwieseV.java
@@ -5,7 +5,7 @@ public class SpielwieseV {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Was geeht");
-		System.out.println("Test Nebenzweig");
+		System.out.println("Test");
 
 	}
 
