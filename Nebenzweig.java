2fbba8496e295aefc69f2b5c0636656876773009
leonwillwersch <2123100@stud.hs-mannheim.de>
2022-03-27 21:14:00 +0200

    Teste Merging!!!

--- Nebenzweig.java
+++ Nebenzweig.java
@@ -4,6 +4,8 @@ public class Nebenzweig {
 	public static void main(String[] args) {
 		System.out.println("NebenzweigTest");
 		System.out.println("TestNebenzweig");
+		
+		System.out.println("Teste Merging!!!!!");
 	}
 
 }
